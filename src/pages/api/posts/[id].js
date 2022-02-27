1bba219bbaeb563ba7eb934607a2b6ed3f218dc6
Iqbal Sahrizal <54559728+iqbal2906@users.noreply.github.com>
2022-02-28 01:12:16 +0700

    method delete and update finished

--- src/pages/api/posts/[id].js
+++ src/pages/api/posts/[id].js
@@ -5,25 +5,43 @@ import Morgan from "morgan";
 dbConnect();
 
 export default async (req, res) => {
-  const { method, body, query: {id} } = req;
+  const {
+    method,
+    body,
+    query: { id },
+  } = req;
   const morgan = Morgan("dev");
 
   switch (method) {
     case "GET":
       try {
         const post = await Post.findById(id);
-        if(!post) return res.status(404).json({msg: "Post doesn't exists"})
+        if (!post) return res.status(404).json({ msg: "Post doesn't exists" });
         await runMiddleware(req, res, morgan);
         return res.status(200).json(post);
       } catch (err) {
         return res.status(400).json({ msg: err.message });
       }
-    case "POST":
+    case "DELETE":
       try {
-        const newPost = new Post(body);
-        const savedPost = await newPost.save();
+        const deletePost = await Post.findByIdAndDelete(id);
+        if (!deletePost)
+          return res.status(404).json({ msg: "Post doesn't exists" });
         await runMiddleware(req, res, morgan);
-        return res.status(200).json(savedPost);
+        return res.status(200).json({ msg: "Post deleted succesfully" });
+      } catch (err) {
+        return res.status(400).json({ msg: err.message });
+      }
+    case "PUT":
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(id, body, {
+          new: true,
+          runValidators: true,
+        });
+
+        if (!updatedPost)
+          return res.status(404).json({ msg: "Post doesn't exists" });
+        return res.status(200).json(updatedPost);
       } catch (err) {
         return res.status(400).json({ msg: err.message });
       }
